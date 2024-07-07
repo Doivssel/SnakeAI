0ca8b8d1d5d5adf3fa9fffb95a8ea6bcfeadebc5
Doivssel <coindet.jeremy@gmail.com>
2024-07-07 14:39:45 +0200

    Update snakeAI.py

--- snakeAI.py
+++ snakeAI.py
@@ -14,11 +14,13 @@ def score(weight,gen_net):
                         [snake.posY],
                         [map.number_col-snake.posX],
                         [map.number_row-snake.posY],
-                        #Apple in the cross direction centerd around the head
-                        [(map.rdm_pos[0]>snake.posX)*1],
-                        [(map.rdm_pos[0]<snake.posX)*1],
-                        [(map.rdm_pos[1]>snake.posY)*1],
-                        [(map.rdm_pos[1]<snake.posY)*1],
+                        #Apple in the cross direction centerd around the head or distance
+                        # [(map.rdm_pos[0]>snake.posX)*1],
+                        # [(map.rdm_pos[0]<snake.posX)*1],
+                        # [(map.rdm_pos[1]>snake.posY)*1],
+                        # [(map.rdm_pos[1]<snake.posY)*1],
+                        [np.abs(snake.posX-map.rdm_pos[0])],
+                        [np.abs(snake.posY-map.rdm_pos[1])],
                         #These are for the current direction 
                         [(snake.direction_lr<0)*1],
                         [(snake.direction_lr>0)*1],
@@ -38,16 +40,16 @@ def score(weight,gen_net):
         elif(dir=="u" and snake.direction_ud==0):
             snake.direction_ud=1
             snake.direction_lr=0
-    return(10*snake.score+snake.time)
+    return(18*snake.score+snake.time)
 
-map_dim,size,function=10,[14,20,20,4],["reLu","reLu","sigmoid"]
+map_dim,size,function=10,[12,20,20,4],["reLu","reLu","sigmoid"]
 
-gen_net=Genetic_Network(size,function,2000)
-gen_net.train(epoch=200,score=score,numb_ind=700,n_c=50,p=0.1,mu=0,sigma=2)
+gen_net=Genetic_Network(size,function,1000)
+gen_net.train(epoch=500,score=score,numb_ind=70,n_c=500,p=0.1,mu=0,sigma=1)
 best_net=gen_net.get_best(score)
-
+print(best_net)
 
 root=tk.Tk()
 net=Network(size,function,best_net)
-game=Game(root,map_dim,80,net)
+game=Game(root,map_dim,200,net)
 game.root.mainloop()
